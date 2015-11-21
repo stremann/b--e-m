973c0b514dfda93272f4b3aac25b62545ea2ba1c
Roman Stremedlovskyi <Roman_Stremedlovskyi@epam.com>
2015-11-21 23:03:06 +0200

    Add first sprites

--- config.rb
+++ config.rb
@@ -2,4 +2,6 @@ http_path = '/'
 css_dir = 'public/css'
 sass_dir = 'lib/css'
 
-require 'breakpoint'
+images_dir = 'lib/img'
+generated_images_dir = 'public/img/sprites'
+http_generated_images_path = '../img/sprites'
