0d02ac2d4c8fbc79b20d15008911ba0751e98d7e
Roman Stremedlovskyi <Roman_Stremedlovskyi@epam.com>
2015-11-24 02:41:55 +0200

    Add style-outline bower package

--- server.js
+++ server.js
@@ -2,10 +2,12 @@
 
 var express = require( 'express' );
 var app = express();
+var path = require ( 'path' );
 
 app.set( 'port', ( process.env.PORT || 3000 ) );
 
 app.use( express.static( __dirname + '/public' ) );
+app.use( '/bower',  express.static( path.join( __dirname, 'vendor/bower' ) ) );
 
 app.set( 'views', __dirname + '/public' );
 app.set( 'view engine', 'ejs' );
